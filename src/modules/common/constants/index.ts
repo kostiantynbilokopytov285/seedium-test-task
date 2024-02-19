b6445f0aae00915bcd5f75bc3fe10502e0aee98a
Kostiantyn Bilokopytov <kostiantynbilokopytov@Kostiantyns-Laptop.local>
2024-02-20 01:14:46 +0200

    [Terms Link]
    changed placeholder urls

--- src/modules/common/constants/index.ts
+++ src/modules/common/constants/index.ts
@@ -1,6 +1,6 @@
 export const HOME_PAGE_VIDEO_LINK =
   'https://www.youtube.com/watch?v=U9rAmaxdvNw'
-export const PRIVACY_POLICY_LINK = 'https://stackoverflow.com/'
+export const PRIVACY_POLICY_LINK = '/'
 export const SALT_OR_ROUNDS = 10
-export const TERMS_OF_SERVICE_LINK = 'https://stackoverflow.com/'
+export const TERMS_OF_SERVICE_LINK = '/'
 export const TOAST_DURATION = 3
